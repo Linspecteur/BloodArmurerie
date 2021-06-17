c895997509cf84c85d2deeeebabb1e3d5bff97ca
L'inspecteur <48238287+Linspecteur@users.noreply.github.com>
2021-06-17 09:58:27 +0200

    Fix erreur accessoires

--- accessoires.sql
+++ accessoires.sql
@@ -1,5 +1,5 @@
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
 ('clip', 'Chargeur', 10, 0, 1),
 ('jumelles', 'Jumelle', 1, 0, 1),
-('kevlar', 'Kevlar', 3, 0, 1),
+('kevlar', 'Kevlar', 3, 0, 1);
 
